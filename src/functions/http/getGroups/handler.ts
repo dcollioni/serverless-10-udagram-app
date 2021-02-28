79924b89a7ff390aa3b5dc1747e38976ff0b53f5
Douglas Collioni <douglascollioni@bluebridgetech.com>
2021-02-28 09:52:20 +0000

    Add aws-serverless-express to get groups

--- src/functions/http/getGroups/handler.ts
+++ src/functions/http/getGroups/handler.ts
@@ -13,3 +13,22 @@ const getGroups: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): P
 }
 
 export const main = middyfy(getGroups);
+
+// import 'source-map-support/register'
+// import { getAllGroups } from './../../../services/groupService'
+
+// import * as express from 'express'
+// import * as awsServerlessExpress from 'aws-serverless-express'
+
+// const app = express()
+
+// app.get('/groups', async (_req, res) => {
+//   const groups = await getAllGroups()
+
+//   res.json({
+//     items: groups
+//   })
+// })
+
+// const server = awsServerlessExpress.createServer(app)
+// export const main = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
